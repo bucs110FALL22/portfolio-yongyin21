9097e9005d6487de648129669cd03040806eeb30
yongyin21 <112529812+yongyin21@users.noreply.github.com>
2022-09-06 21:50:41 +0000

    excercise working with data

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,19 @@
+print(5/2)
+print(10**5)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+
+rate = input("What is the current exchange rate for the Euro to Dollar?")
+amount= input("What is the amount of currency you want to exchange?")
+
+total =(float (rate)*float(amount))
+
+
+result = float(total-3)
+print(result)
