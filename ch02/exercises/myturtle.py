debe5cc2cb06d293cdf1717ea3b4aa55e794f8c0
yongyin21 <112529812+yongyin21@users.noreply.github.com>
2022-09-07 22:09:18 +0000

    turtle

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,32 @@
+import turtle              
+my_turtle = turtle.Turtle()
+my_turtle.color("purple")
+#my_turtle.forward(50)
+my_turtle.shape("turtle")
+#my_turtle.left(100)
+num_sides = int(input("Please enter the number of sides"))
+length = 50 
+angle = 360 / num_sides
+
+for i in [angle]*num_sides:
+
+        my_turtle.forward(length)
+        my_turtle.left(i)  
+#my_turtle.left(90)
+my_turtle.color("red")
+
+my_turtle.penup
+my_turtle.forward(50)
+my_turtle.pendown
+num_sides = int(input("Please enter the number of sides"))
+length = 50 
+angle = 360 / num_sides
+
+for i in [angle]*num_sides:
+
+        my_turtle.forward(length)
+        my_turtle.left(i)  
+
+#my_turtle.forward(30)
+#my_turtle.color("yellow")
+turtle.done()
