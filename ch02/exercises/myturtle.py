f60331d798c6c43682219ca9016bb579d3f87048
yongyin21 <112529812+yongyin21@users.noreply.github.com>
2022-09-07 22:16:58 +0000

    turtle exercise attempt

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -16,7 +16,8 @@ for i in [angle]*num_sides:
 my_turtle.color("red")
 
 my_turtle.penup
-my_turtle.forward(50)
+my_turtle.forward(100)
+
 my_turtle.pendown
 num_sides = int(input("Please enter the number of sides"))
 length = 50 
