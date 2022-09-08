82b9e28f4021abd1a05376784d8495c340dc687a
yongyin21 <112529812+yongyin21@users.noreply.github.com>
2022-09-08 22:46:47 +0000

    lab 2 attempy

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,18 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+classes_per_week = 3
+print("Classes per week:", classes_per_week)
+cost_per_class = ((cost_per_week/classes_per_week))
+print("Your incredibly high cost per class:",cost_per_class) #This "cost_per_class is how much you a class would cost based on tuition and number of classes a week"
 
 
 #Part B
+list [5, 6, 7, 8, 9]
+random.choice(list)
+randys = random.choice(list)
+print(randys)
